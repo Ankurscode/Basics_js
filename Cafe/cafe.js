c21f5e0723f0fa806f161248ff0df860c3db77e7
Ankur yadav <165983779+Ankurscode@users.noreply.github.com>
2024-09-12 14:29:50 +0530

    Add files via upload

---
+++ Cafe/cafe.js
@@ -0,0 +1,16 @@
+ let cafe={
+    coffee:5,
+    bagels:3,
+    soup:9
+ }
+ let tax=10/100;
+
+ let cost=cafe.coffee+cafe.bagels*2+cafe.soup;
+ console.log(`Cost of food:$${cost}`);
+ let withtax=cost*0.1;
+ console.log(`Tax of this is:$${withtax}`);
+ let totalcost=cost+withtax;
+ console.log(`Total cost including tax:$${totalcost}`);
+
+ 
+
